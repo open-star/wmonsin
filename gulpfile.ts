4342cdebcc4692380a8452a020c48df2244bfc13
oda <oda.mikio@gmail.com>
2016-02-11 15:40:46 +0900

    gulp

--- gulpfile.ts
+++ gulpfile.ts
@@ -4,17 +4,8 @@ declare function require(x:string):any;
 
 var gulp = require('gulp');
 
-var gutil = require('gulp-util');
-var ftp = require('vinyl-ftp');
-var ts = require('gulp-typescript');
-var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
-var rename = require("gulp-rename");
-var rimraf = require('rimraf');
-var less = require('gulp-less');
-var minifycss = require('gulp-minify-css');
+gulp.task('default', [], () => {
 
-gulp.task('copy', [], () => {
     return gulp.src(
         [
             'init.js',
@@ -43,61 +34,4 @@ gulp.task('copy', [], () => {
         {base: '..'}
     )
         .pipe(gulp.dest('../womnsin_image'));
-});
-
-gulp.task('css', () => {
-    return gulp.src('public/**/stylesheets/*.less')
-        .pipe(less())
-        .pipe(minifycss())
-        .pipe(gulp.dest('production/wmonsin/public'));
-});
-
-gulp.task('js', () => {
-    return gulp.src(
-        [
-            'public/javascripts/*.js',
-            'public/backend/javascripts/*.js',
-            'public/front/javascripts/*.js'
-        ],
-        {base: '..'})
-        .pipe(uglify())
-        .pipe(rename({extname: '.min.js'}))
-        .pipe(gulp.dest('production'));
-});
-
-gulp.task('clean', (cb) => {
-    rimraf('production', cb);
-});
-
-gulp.task('jsconcat', () => {
-    return gulp.src(
-        [
-            'public/javascripts/*.js',
-            'public/backend/javascripts/*.js',
-            'public/front/javascripts/*.js'
-        ],
-        {base: '..'})
-        .pipe(uglify())
-        .pipe(concat('client.min.js'))
-        .pipe(gulp.dest('production/wmonsin/public/javascripts'))
-        .pipe(gulp.dest('public/javascripts'));
-});
-
-gulp.task('ftp', () => {
-
-    var conn = ftp.create({
-        host: '128.199.196.240',
-        user: '',
-        pass: '',
-        parallel: 1,
-        log: gutil.log
-    });
-
-    return gulp.src(['production/**'], {base: 'app/wmonsin', buffer: false})
-        .pipe(conn.newer('/'))
-        .pipe(conn.dest('/'));
-});
-
-gulp.task('default', ['copy', 'css', 'js'], () => {
-    console.log('done');
 });
